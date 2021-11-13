e6db6c83cc8828379e7b614e5051d452db804629
MarinaGherman <marina.gherman@icloud.com>
2021-11-13 17:59:48 +0100

    add project 🎉

---
+++ src/common/header/nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <a href="">Main</a>
+            <a href="">Skills</a>
+            <a href="">Projects</a>
+            <a href="">Contacts</a>
+        </div>
+    );
+};
+
+export default Nav;
