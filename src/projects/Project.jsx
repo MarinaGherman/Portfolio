b475706414c8a5ab3a7809ee2f0324120f424827
marina <marina.gherman@icloud.com>
2022-02-04 13:27:00 +0100

    add my projects

--- src/projects/Project.jsx
+++ src/projects/Project.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import s from './Project.module.css'
 
-const Project = (props) => {
+const Project = ({image, title, text, link}) => {
     return (
         <div className={s.projectBlock}>
             <div className={s.bgFrame}>
-                <div className={s.image}>
+                <div className={s.image}
+                     style={{backgroundImage: "url(" + image + ")"}}>
                     <div className={s.btnBlock}>
-                        <button className={s.btn}>Open project</button>
+                        <button className={s.btn}>
+                            <a target="_blank" href={link}>Open Link</a>
+                        </button>
                     </div>
                 </div>
+                <p>{title}</p>
+                <span>{text}</span>
             </div>
         </div>
     );
--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import s from './Projects.module.css'
 import Project from "./Project";
 
+const lingvaninja  = "https://lingvoninja.com//images/meta/index.png";
+const zodier = "https://zodier.io/static/meta-image.png";
+const lingvaninjaText = "Lingvo Ninja — this is a set of short 10-minute lessons for quick and easy learning of English and English. The service works in Telegram, which allows you to learn English in the usual messenger without downloading individual applications.";
+const lingvaninjaTitle = "Lingvo Ninja"
+const zodierText = "Zodier simplifies this process and allows you to meet people and chat in popular instant messengers. This is a bot that determines the compatibility between people using a special algorithm and helps to find the most suitable person for friendship or relationship."
+const zodierTitle = "Zodier"
+const zodierLink = "https://zodier.io/en#contact"
+const lingvoNinjaLink = "https://lingvoninja.com/"
+
+
 const Projects = () => {
     return (
         <div className={s.projectsBlock} id="projects">
@@ -9,8 +19,16 @@ const Projects = () => {
                 <h2>My Projects</h2>
             </div>
             <div className={s.projectComponents}>
-                <Project/>
-                <Project/>
+                <Project image={lingvaninja}
+                         title={lingvaninjaTitle}
+                         text ={lingvaninjaText}
+                         link={lingvoNinjaLink}
+                />
+                <Project image={zodier}
+                         title={zodierTitle}
+                         text ={zodierText}
+                         link={zodierLink}
+                />
             </div>
         </div>
     );
